f58462f821a8b293177ecc713a67e160f4cb3412
Lunski <eric999j@gmail.com>
2019-08-27 20:40:07 +0800

    Add files via upload

---
+++ FirstDetection.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+不用WHL，要下載訓練好的模型resnet50_coco_best_v2.0.1.h5  
+
+"""
+from imageai.Detection import ObjectDetection
+import os
+
+
+execution_path = os.getcwd()
+
+
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
+
+
+for eachObject in detections:
+   print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
